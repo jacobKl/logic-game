67a65d46f0db90e6be34be613b1643ac2978b99a
Klimek Jakub <jklimek@MacBook-Pro-Klimek.local>
2021-06-07 17:31:17 +0200

    SERVER: missing RoomsManager ext

--- server/components/RoomsManager.js
+++ server/components/RoomsManager.js
@@ -13,23 +13,23 @@ class RoomsManager {
     rooms.push({
       id: rooms.length,
       inGame: 0,
-      playersData: [
-        { x: 0, y: 0, z: 0 },
-        { x: 0, y: 0, z: 0 },
+      gameData: [
+        { x: 0, y: 0, z: 0, lookAt: { x: 0, y: 0, z: 0 }, animation: "" },
+        { x: 0, y: 0, z: 0, lookAt: { x: 0, y: 0, z: 0 }, animation: "" },
       ],
     });
     return rooms[rooms.length - 1];
   }
 
   changePlayerData(gameId, playerId, data) {
-    rooms[gameId].playersData[playerId] = data;
+    rooms[gameId].gameData[playerId] = data;
   }
 
   getEnemyData(gameId, playerId) {
     let enemyId = 0;
     if (playerId == 2) enemyId = 0;
     if (playerId == 1) enemyId = 1;
-    return rooms[gameId].playersData[enemyId];
+    return rooms[gameId].gameData[enemyId];
   }
 }
 
